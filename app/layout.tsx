6db7477c71078b3f3812b237e2a4d2457ddbd98d
Sujal3231 <Sujal.tejlite@gmail.com>
2025-10-16 17:03:33 +0530

    changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ import Header from "./components/Header";
 import ReduxProvider from "./components/ReduxProvider";
 import Footer from "./components/Footer";
 import { ReactNode } from "react";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,15 +18,9 @@ interface RootLayoutProps {
   children: ReactNode;
 }
 
-export default function RootLayout({ children }: RootLayoutProps): JSX.Element {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-        />
-      </Head>
       <body className={inter.className}>
         <ReduxProvider>
           <Header />
